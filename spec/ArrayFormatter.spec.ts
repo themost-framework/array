38036318ae269fff057eb4df798fe1c4c2fd5cc6
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-05-07 17:33:41 +0000

    add functions

--- spec/ArrayFormatter.spec.ts
+++ spec/ArrayFormatter.spec.ts
@@ -1,8 +1,8 @@
-import { QueryExpression } from '@themost/query';
+import { QueryExpression, QueryField } from '@themost/query';
 import { ArrayFormatter } from '@themost/array';
 import { PRODUCTS } from './data/Products';
 describe('ArrayFormatter', () => {
-    it('should parse select', async () => {
+    it('should format select', async () => {
         const q = new QueryExpression().select('id', 'name').from('Products');
         const formatter = new ArrayFormatter();
         const selectMap: (item: any) => any = formatter.formatSelect(q);
@@ -19,4 +19,77 @@ describe('ArrayFormatter', () => {
         expect(items[0].id).toEqual(PRODUCTS[0].id);
         expect(items[0].name).toEqual(PRODUCTS[0].name);
     });
+
+    it('should format select with alias', async () => {
+        const q = new QueryExpression().select(
+            new QueryField('id'),
+            new QueryField('name').as('productName')
+        ).from('Products');
+        const formatter = new ArrayFormatter();
+        const selectMap: (item: any) => any = formatter.formatSelect(q);
+        const items = PRODUCTS.map((item) => {
+            return selectMap(item);
+        })
+        expect(items).toBeTruthy();
+        for (const item of items) {
+            const keys = Object.keys(item);
+            expect(keys).toEqual([
+                'id', 'productName'
+            ]);
+        }
+        expect(items[0].id).toEqual(PRODUCTS[0].id);
+        expect(items[0].productName).toEqual(PRODUCTS[0].name);
+    });
+
+    it('should use escape', () => {
+        const formatter = new ArrayFormatter();
+        const expr:any = formatter.escape(new QueryField('id'));
+        expect(expr).toBeTruthy();
+        expect(formatter.escape(true, true)).toBeTrue()
+        expect(expr).toBeInstanceOf(Function);
+        expect(expr({
+            id: 10
+        })).toEqual(10);
+    })
+
+    it('should format select with function', async () => {
+        const q = new QueryExpression().select(
+            new QueryField('id'),
+            QueryField.round('price', 1).as('price')
+        ).from('Products');
+        const formatter = new ArrayFormatter();
+        const selectMap: (item: any) => any = formatter.formatSelect(q);
+        const items = PRODUCTS.map((item) => {
+            return selectMap(item);
+        })
+        expect(items).toBeTruthy();
+        for (const item of items) {
+            const keys = Object.keys(item);
+            expect(keys).toEqual([
+                'id', 'price'
+            ]);
+        }
+        expect(items[0].id).toEqual(PRODUCTS[0].id);
+        expect(items[0].price).toBeTruthy();
+    });
+
+    it('should format where', async () => {
+        const q = new QueryExpression()
+            .select('id', 'name').from('Products').where('id').equal(1);
+        const formatter = new ArrayFormatter();
+        const selectMap: (item: any) => any = formatter.formatSelect(q);
+        const items = PRODUCTS.map((item) => {
+            return selectMap(item);
+        })
+        expect(items).toBeTruthy();
+        for (const item of items) {
+            const keys = Object.keys(item);
+            expect(keys).toEqual([
+                'id', 'name'
+            ]);
+        }
+        expect(items[0].id).toEqual(PRODUCTS[0].id);
+        expect(items[0].name).toEqual(PRODUCTS[0].name);
+    });
+
 });
--- src/ArrayFormatter.ts
+++ src/ArrayFormatter.ts
@@ -1,5 +1,6 @@
 import { QueryExpression, QueryField, SqlFormatter } from '@themost/query';
 import { pick, at } from 'lodash';
+
 function getOwnPropertyName(obj: any) {
     if (obj == null) {
         return null;
@@ -12,6 +13,23 @@ function getOwnPropertyName(obj: any) {
     return null;
 }
 
+function round(n: any, precision?: number) {
+    if (typeof n !== 'number') {
+        return 0;
+    }
+    if (precision) {
+        return parseFloat(n.toFixed(precision))
+    }
+    return Math.round(n);
+}
+
+function mapValue(expr: any, source: any): any {
+    if (typeof expr === 'function') {
+        return expr(source);
+    }
+    return expr;
+}
+
 class ArrayFormatter extends SqlFormatter {
     constructor() {
         super();
@@ -19,6 +37,16 @@ class ArrayFormatter extends SqlFormatter {
 
     escapeConstant(value: any,unquoted?: boolean): any {
         return value;
+    }
+
+    escape(value: any, unquoted?: boolean): any {
+        return super.escape(value, unquoted);
+    }
+
+    escapeName(name: string): any {
+        return (item: any) => {
+            return at(item, name)[0];
+        }
     }
 
     formatField(obj: any): any {
@@ -55,11 +83,30 @@ class ArrayFormatter extends SqlFormatter {
                     at(item, field[property])[0]
                 ];
             }
+        } else {
+            const exprFunc = this[property];
+            if (typeof exprFunc === 'function') {
+                return exprFunc.apply(this, [].concat(field[property]));
+            }
+            return (item: any) => {
+                const value = this.format(field[property], '%f');
+                if (typeof value === 'function') {
+                    const finalValue = value(item);
+                    return [
+                        property,
+                        Array.isArray(finalValue) ? finalValue[1] : finalValue
+                    ]
+                }
+                return [
+                    property,
+                    value
+                ]
+            }
         }
     }
 
     format(obj: any, s?: string): any {
-        return super.format(obj);
+        return super.format(obj, s);
     }
 
     formatSelect(query: QueryExpression | any): any {
@@ -71,11 +118,7 @@ class ArrayFormatter extends SqlFormatter {
                 const attributes = select[key];
                 const selectExpressions: any[] = [];
                 for (const attribute of attributes) {
-                    if (typeof attribute === 'string') {
-                        selectExpressions.push(this.formatFieldEx(new QueryField(attribute), '%f'));
-                    } else {
-                        selectExpressions.push(this.formatFieldEx(attribute, '%f'));
-                    }
+                    selectExpressions.push(this.format(attribute, '%f'));
                 }
                 // return map closure
                 return (item:any) => {
@@ -83,16 +126,251 @@ class ArrayFormatter extends SqlFormatter {
                         // execute select
                         const selectResult: any[] = selectExpression(item);
                         // and set property
+                        let selectResult1: any;
+                        if (typeof selectResult[1] === 'function') {
+                            selectResult1 = selectResult[1](item);
+                        } else {
+                            selectResult1 = selectResult[1];
+                        }
                         Object.defineProperty(value, selectResult[0], {
                             configurable: true,
                             enumerable: true,
                             writable: true,
-                            value: selectResult[1]
+                            value: selectResult1
                         });
                         return value;
                     }, {});
                 }
             }
+        }
+    }
+
+    $length(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value && String(value).length;
+        }
+    }
+
+    $substr(p0: any, pos: number, length?: number): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value && String(value).substr(pos, length);
+        }
+    }
+
+    $substring(p0: any, pos: number, length?: number): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value && String(value).substring(pos, length);
+        }
+    }
+
+    $trim(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value && String(value).trim();
+        }
+    }
+
+    $tolower(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value && String(value).toLocaleLowerCase();
+        }
+    }
+
+    $toupper(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value && String(value).toLocaleUpperCase();
+        }
+    }
+
+    $startswith(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return String(value1).startsWith(String(value2));
+        }
+    }
+
+    $endswith(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return String(value1).endsWith(String(value2));
+        }
+    }
+
+    $concat(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return String(value1).concat(String(value2));
+        }
+    }
+
+    $indexof(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return String(value1).indexOf(String(value2));
+        }
+    }
+
+    $contains(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return (String(value1).indexOf(String(value2)) >= 0);
+        }
+    }
+
+    $text(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return (String(value1).match(String(value2)) != null);
+        }
+    }
+
+    $day(p0:any): any {
+        return (item: any) => {
+            const result: any = this.escape(p0);
+            const value: any = (typeof result === 'function') ? result(item) : result;
+            return value instanceof Date && value.getDate();
+        }
+    }
+
+    $dayOfMonth(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value instanceof Date && value.getDate();
+        }
+    }
+
+    $month(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value instanceof Date && value.getMonth() + 1;
+        }
+    }
+
+    $year(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value instanceof Date && value.getFullYear();
+        }
+    }
+
+    $hour(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value instanceof Date && value.getHours();
+        }
+    }
+
+    $minute(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value instanceof Date && value.getMinutes();
+        }
+    }
+
+    $second(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value instanceof Date && value.getSeconds();
+        }
+    }
+
+    $date(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return value instanceof Date && new Date(value.getFullYear(), value.getMonth(), value.getDate());
+        }
+    }
+
+    $round(p0:any, precision?: any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return round(value, precision);
+        }
+    }
+
+    $floor(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return Math.floor(value,);
+        }
+    }
+
+    $ceiling(p0:any): any {
+        return (item: any) => {
+            const value: any = mapValue(this.escape(p0), item);
+            return Math.ceil(value,);
+        }
+    }
+
+    $add(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return value1 + value2;
+        }
+    }
+
+    $subtract(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return value1 - value2;
+        }
+    }
+
+    $sub(p0:any, p1:any): any {
+        return this.$subtract(p0, p1);
+    }
+
+    $multiply(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return value1 * value2;
+        }
+    }
+
+    $mul(p0:any, p1:any): any {
+        return this.$multiply(p0, p1);
+    }
+
+    $divide(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return value1 * value2;
+        }
+    }
+
+    $div(p0:any, p1:any): any {
+        return this.$divide(p0, p1);
+    }
+
+    $mod(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            return value1 % value2;
+        }
+    }
+
+    $bit(p0:any, p1:any): any {
+        return (item: any) => {
+            const value1: any = mapValue(this.escape(p0), item);
+            const value2: any = mapValue(this.escape(p1), item);
+            // tslint:disable-next-line: no-bitwise
+            return value1 & value2;
         }
     }
 
